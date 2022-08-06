007a1bcc2f65036a535d23a0c76b04605034479b
minhnhatnoe <86871862+minhnhatnoe@users.noreply.github.com>
2022-08-06 21:30:07 +0700

    __init__ for class Submission

---
+++ src/api/classes.py
@@ -0,0 +1,14 @@
+import datetime
+class Submission:
+    def __init__(self, lang: str, contestant: str, problem_name: str,
+                source: str,
+                submit_timestamp: datetime.datetime,
+                recieve_timestamp: datetime.datetime = datetime.datetime.now()) -> None:
+        '''Creates a new submission.
+        Lang: extension of code.
+        Source: Information regarding source of submission.
+        Submit_timestamp: Timestamp retrieved from the respective service'''
+        self.lang, self.contestant, self.problem_name = lang, contestant, problem_name
+        self.source = source
+        self.submit_timestamp, self.recieve_timestamp = submit_timestamp, recieve_timestamp
+
