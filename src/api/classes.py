dcb964335706e6c1767762633a977c7fa6fd0419
minhnhatnoe <86871862+minhnhatnoe@users.noreply.github.com>
2022-08-07 17:24:14 +0700

    Code to run validators

--- src/api/classes.py
+++ src/api/classes.py
@@ -1,16 +1,19 @@
+'''Helper classes to interact with Themis'''
 from os import path
 import logging
 import datetime
 import hashlib
-from typing import List
+from typing import Callable, List
 from src import config
 from src.api import fileio
 
+
 class Submission:
+    '''Represents a submission'''
     def __init__(self, contestant: str, problem_name: str, lang: str,
-                content: str, source: str,
-                submit_timestamp: datetime.datetime,
-                recieve_timestamp: datetime.datetime = datetime.datetime.now()) -> None:
+                 content: str, source: str,
+                 submit_timestamp: datetime.datetime,
+                 recieve_timestamp: datetime.datetime = datetime.datetime.now()) -> None:
         '''Creates a new submission.
         Lang: extension of code.
         Source: Information regarding source of submission.
@@ -19,8 +22,9 @@ class Submission:
         self.content = content
         self.source = source
         self.submit_timestamp, self.recieve_timestamp = submit_timestamp, recieve_timestamp
-        logging.info(f"Recieved {problem_name}.{lang} of {contestant} from {source} at {recieve_timestamp}")
-    
+        logging.info(
+            f"Recieved {problem_name}.{lang} of {contestant} from {source} at {recieve_timestamp}")
+
     def __hash__(self) -> str:
         '''Hashes a submission.
         The same submission from a platform (ie. Same cell in Sheets)\
@@ -36,42 +40,67 @@ class Submission:
     def get_file_name(self) -> str:
         return f"{hash(self)}[{self.contestant}][{self.problem_name}].[{self.lang}]"
 
+
 class ThemisInteractError(Exception):
     '''Class for throwing Themis errors around'''
 
-class CodeError(Exception):
-    '''Class for throwing errors with codes submitted'''
 
-class BannedSyntax(CodeError):
-    '''Thrown if specified pattern appears in code'''
+class CodeError(Exception):
+    '''Base class for throwing errors with codes submitted'''
+
+    def __init__(self, console_msg: str, contestant_msg: str|None = None) -> None:
+        '''Console_msg will be printed to console and log.\
+           Contestant_msg will be sent to contestant, defaults to console_msg'''
+        super().__init__()
+        self.console_msg = console_msg
+        self.contestant_msg = console_msg if contestant_msg is None else contestant_msg
+
 
 class DuplicatedCodeError(CodeError):
-    '''Thrown if user submitted duplicate code'''
+    '''Class for throwing when code is duplicated'''
+
 
 class ThemisInstance:
+    '''The one and only object needed to interact with Themis.'''
+
     def __init__(self, osd: str, contestants: List[str],
-                 skip_duplicate: config.SKIP_DUPLICATE) -> None:
-        '''The one and only object needed to interact with Themis.'''
+                 validators: List[Callable[[Submission], None]]) -> None:
+        '''Initializes the instance with options'''
         if not path.exists(osd):
-            raise FileNotFoundError(f"{osd} cannot be used because it doesn't exists.")
+            raise FileNotFoundError(
+                f"{osd} cannot be used because it doesn't exists.")
         self.osd = osd
-        self.skip_duplicate = skip_duplicate
         self.contestants = {contestant: {} for contestant in contestants}
+        self.validators = validators
         logging.info(f"Created Instance writing to {self.osd}")
-    
+
     def is_contestant(self, name: str) -> bool:
+        '''Checks whether a name is a contestant of this instance'''
         return name in self.contestants
 
-    def submission_legit(self, submission: Submission) -> None:
-        if hash(submission.content) in self.contestants[submission.contestant]:
-            raise CodeError("Duplicated code")
-        
-    
+    def validate_submission(self, sub: Submission) -> None:
+        '''Checks if a submission is legitimate'''
+        if hash(sub.content) in self.contestants[sub.contestant]:
+            logging.warning(
+                f"{sub.contestant}'s code of {sub.problem_name} is duplicated.")
+            raise DuplicatedCodeError("Duplicated code.", contestant_msg="You submitted duplicated code")
+        for validator in self.validators:
+            try:
+                validator(sub)
+            except CodeError as error:
+                logging.warning(
+                    f"Validator {validator.__name__} denied \
+{sub.contestant}'s code of {sub.problem_name} due to {error.console_msg}")
+                raise error
+
     async def submit(self,
-                     submission: Submission,
+                     sub: Submission,
                      await_result: bool = config.ONLINE_MODE) -> None | str:
         '''Submits to Themis for judging. Returns the result if await_result is True'''
 
-        fileio.submit(self.osd, submission)
-        if not await_result: return None
-        return await fileio.read_result(self.osd, submission)
+        assert(self.is_contestant(sub.contestant))
+        self.validate_sub(sub)
+        fileio.submit(self.osd, sub)
+        if not await_result:
+            return None
+        return await fileio.read_result(self.osd, sub)
