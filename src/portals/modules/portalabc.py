2c740a632b57433e9941f40d9cdeb94ea438eb67
Unknown15082 <trangiahuy15082006@gmail.com>
2022-08-08 20:56:38 +0700

    Fix typos (recieve -> receive)

--- src/portals/modules/portalabc.py
+++ src/portals/modules/portalabc.py
@@ -9,29 +9,29 @@ class Portal(ABC):
     """Base class for all portals"""
     name = "Base"
 
-    def __init__(self, on_recieve: Callable[[Submission], Coroutine[str]]):
-        """Function on_recieve will be called whenever a new submission is detected"""
-        self.on_recieve = on_recieve
+    def __init__(self, on_receive: Callable[[Submission], Coroutine[str]]):
+        """Function on_receive will be called whenever a new submission is detected"""
+        self.on_receive = on_receive
 
 
 class PassivePortal(Portal, ABC):
     """All passive portals should inherit from this class"""
 
-    def __init__(self, on_recieve: Callable[[Submission], Coroutine[str]]):
-        """Function on_recieve will be called whenever a new submission is detected"""
+    def __init__(self, on_receive: Callable[[Submission], Coroutine[str]]):
+        """Function on_receive will be called whenever a new submission is detected"""
         logging.info("%s portal has been initialized", type(self).name)
-        super().__init__(on_recieve)
+        super().__init__(on_receive)
 
 
 class ActivePortal(Portal, ABC):
     """All active portals should inherit from this class"""
 
-    def __init__(self, on_recieve: Callable[[Submission], Coroutine[str]],
+    def __init__(self, on_receive: Callable[[Submission], Coroutine[str]],
                  period: float):
         """Period is number of seconds to wait between crawling attempts"""
         logging.info(
             "%s portal has been initialized, crawling every %fs", type(self).name, period)
-        super().__init__(on_recieve)
+        super().__init__(on_receive)
         self.period = period
 
     async def crawl(self):
