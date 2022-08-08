7f12fb3f3caa5f472305725505d365e0387de2d9
minhnhatnoe <86871862+minhnhatnoe@users.noreply.github.com>
2022-08-08 18:00:26 +0700

    Pylint conform

--- src/portals/modules/manual.py
+++ src/portals/modules/manual.py
@@ -1,2 +1,1 @@
 '''Manual submission mode'''
-
--- src/portals/modules/portalabc.py
+++ src/portals/modules/portalabc.py
@@ -4,32 +4,39 @@ import logging
 from typing import Callable, Coroutine
 from src.api.submission import Submission
 
+
 class Portal(ABC):
     """Base class for all portals"""
     name = "Base"
+
     def __init__(self, on_recieve: Callable[[Submission], Coroutine[None]]):
         """Function on_recieve will be called whenever a new submission is detected"""
         self.on_recieve = on_recieve
 
+
 class PassivePortal(Portal, ABC):
     """All passive portals should inherit from this class"""
+
     def __init__(self, on_recieve: Callable[[Submission], Coroutine[None]]):
         """Function on_recieve will be called whenever a new submission is detected"""
         logging.info("{} portal has been initialized", type(self).name)
         super().__init__(on_recieve)
 
+
 class ActivePortal(Portal, ABC):
     """All active portals should inherit from this class"""
+
     def __init__(self, on_recieve: Callable[[Submission], Coroutine[None]],
                  period: float):
         """Period is number of seconds to wait between crawling attempts"""
-        logging.info("{} portal has been initialized, crawling every {}s", type(self.name), period)
+        logging.info(
+            "{} portal has been initialized, crawling every {}s", type(self.name), period)
         super().__init__(on_recieve)
         self.period = period
 
     def crawl(self):
         """Should be implemented by subclasses. Simply starts crawling"""
-    
+
     def start_loop(self):
         """Starts looping. This will call self.crawl periodically."""
         # TODO
