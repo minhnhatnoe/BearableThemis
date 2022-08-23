42f46cc5a2aa0874c7ccd45c0b9188553d9984ba
minhnhatnoe <86871862+minhnhatnoe@users.noreply.github.com>
2022-08-23 16:40:20 +0700

    Base class

--- src/portals/modules/portalabc.py
+++ src/portals/modules/portalabc.py
@@ -1,47 +1,18 @@
 """Abstract Base Class for portals"""
 from abc import ABC
-import asyncio
-import logging
-from typing import Any, Callable, Coroutine
+from typing import AsyncGenerator
 from api.submission import Submission
 
-
 class Portal(ABC):
-    """Base class for all portals"""
-    name = "Base"
+    def __init__(self) -> None:
+        """Initializes the Portal with respective arguments"""
 
-    def __init__(self, on_receive: Callable[[Submission], Coroutine[Any, Any, str]]):
-        """Function on_receive will be called whenever a new submission is detected"""
-        self.on_receive = on_receive
-
-
-class PassivePortal(Portal, ABC):
-    """All passive portals should inherit from this class"""
-
-    def __init__(self, on_receive: Callable[[Submission], Coroutine[Any, Any, str]]) -> None:
-        """Function on_receive will be called whenever a new submission is detected"""
-        logging.info("%s portal has been initialized", type(self).name)
-        super().__init__(on_receive)
-
-    async def start(self) -> None:
-        """Start listening."""
-
-class ActivePortal(Portal, ABC):
-    """All active portals should inherit from this class"""
-
-    def __init__(self, on_receive: Callable[[Submission], Coroutine[Any, Any, str]],
-                 period: float) -> None:
-        """Period is number of seconds to wait between crawling attempts"""
-        logging.info(
-            "%s portal has been initialized, crawling every %fs", type(self).name, period)
-        super().__init__(on_receive)
-        self.period = period
-
-    async def crawl(self) -> None:
-        """Should be implemented by subclasses. Simply starts crawling"""
-
-    async def start(self) -> None:
-        """Starts looping. This will call self.crawl periodically."""
+    async def listen(self) -> AsyncGenerator[Submission, None]:
+        """Starts listening to codes. Overwrite for active portals."""
         while True:
-            self.crawl()
-            asyncio.sleep(self.period)
+            new_subs = self.manual_fetch()
+            for subs in new_subs:
+                yield subs
+    
+    def manual_fetch(self) -> list[Submission]:
+        """Check source for code. Overwrite for passive portals."""
